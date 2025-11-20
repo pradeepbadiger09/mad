825c0ac15712a9e74cd38c3127e96d90cf30731a
pradeepbadiger09 <pradeepbadiger618@gmail.com>
2025-11-20 14:54:50 +0530

    Update stddetails.py

--- stddetails.py
+++ stddetails.py
@@ -3,7 +3,7 @@ if len(sys.argv) !=3:
   print("Uage: python student.py <name> <rollno>")
   sys.exit(1)
 script_name = sys.argv[0]
-name = sys.aryv[1]
+name = sys.argv[1]
 rollno = sys.argv[2]
 print("Script Name:", script_name)
 print("Student name:", name)
