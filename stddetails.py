ac1e35013677c4284a11df5354d250f7a78f6fbb
pradeepbadiger09 <pradeepbadiger618@gmail.com>
2025-11-20 14:07:55 +0530

    Create stddetails.py

---
+++ stddetails.py
@@ -0,0 +1,10 @@
+import sys
+if len(sys.argv) 1=3
+prit("Uage: python student.py <name> <roll no>
+     sys.exit(1)
+     script_name = sys.argv[0]
+name = sys.aryv[1]
+rollno = sys.argv[2]
+print("Script Name:", script_name)
+print(Student name:", name)
+print("Roll Number:", rollno)
